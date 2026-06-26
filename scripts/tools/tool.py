acc89261ee3422ce3f5438d1ce11dfa7c39c0920
kamalrawat77 <kamalrawat77@gmail.com>
2026-06-26 10:51:25 -0700

    Create tool.py

---
+++ scripts/tools/tool.py
@@ -0,0 +1,12 @@
+class Tool:
+
+    def __init__(
+        self,
+        name,
+        description,
+        function
+    ):
+
+        self.name = name
+        self.description = description
+        self.function = function
