718bb95250fa6e657ca6f6b46c3acc7e73a10596
kamalrawat77 <kamalrawat77@gmail.com>
2026-06-28 09:22:50 -0700

    Update decorators.py

--- scripts/tools/decorators.py
+++ scripts/tools/decorators.py
@@ -3,20 +3,35 @@ from scripts.tools.registry import registry
 from scripts.tools.tool import Tool
 
 def tool(description: str, category: str):
+    
+    TYPE_MAP = {
+        int: "integer",
+        float: "number",
+        bool: "boolean",
+        str: "string",
+        list: "array",
+        dict: "object"
+    }
 
     def decorator(func):
-
         sig = signature(func)
-
+        
         parameters = {}
-
+        
         for name, param in sig.parameters.items():
-
+        
+            annotation = param.annotation
+        
+            json_type = TYPE_MAP.get(annotation, "string")
+        
+            required = param.default is param.empty
+        
+            default = None if required else param.default
+        
             parameters[name] = {
-                "type": "string",   # We'll improve this in Nugget 052
-                "default": (
-                    None if param.default is param.empty else param.default
-                )
+                "type": json_type,
+                "required": required,
+                "default": default
             }
 
         tool_obj = Tool(
